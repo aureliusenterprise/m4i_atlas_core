6f716041b80b00b322e8823688e1a169cc2fabeb
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-20 07:12:09 +0000

    rewrote and now works

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-pip install -e .[dev]
+
+# Install workspace in editable mode
+pip install --user -e /workspace[dev]
+
+# Copy config and credentials sample files
 cp "./scripts/config.sample.py" "./scripts/config.py"
 cp "./scripts/credentials.sample.py" "./scripts/credentials.py"
