e5d041c23a1ee23e53ad790b99bfb291655513a1
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-20 06:23:30 +0000

    removed comma from sample file

--- scripts/config.sample.py
+++ scripts/config.sample.py
@@ -1,3 +1,3 @@
 config = {
-    "atlas.server.url": "YOUR_SERVER_URL (e.g. http://atlas.models4insight.com/api/atlas)",
+    "atlas.server.url": "YOUR_SERVER_URL (e.g. http://atlas.models4insight.com/api/atlas)"
 }
