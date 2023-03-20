dee372dd4f415a57f70e1fa660cabe35befd7d18
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-20 06:37:58 +0000

    put cp command params in quotes

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,8 +1,8 @@
 # Install the package including all development dependencies
-pip install -e .[dev] --user
+pip install -e .[dev] --user 
 
 # Copy the config sample file
-cp ./scripts/config.sample.py ./scripts/config.py
+cp "./scripts/config.sample.py" "./scripts/config.py" 
 
 # Copy the credentials sample file
-cp ./scripts/credentials.sample.py ./scripts/credentials.py
+cp "./scripts/credentials.sample.py" "./scripts/credentials.py" 
