7418c3a7620742a46533b74638456a8fff23dde8
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-20 06:56:47 +0000

    more changes

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Install the package including all development dependencies
-pip install --user -e .[dev] 
-
-# Copy the config sample file
+pip install -e .[dev]
 cp "./scripts/config.sample.py" "./scripts/config.py"
-
-# Copy the credentials sample file
 cp "./scripts/credentials.sample.py" "./scripts/credentials.py"
