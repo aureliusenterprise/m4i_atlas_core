689835e7c17533a606b8565783a5a9170aae84e0
Thijs Franck <37906715+thijs-franck@users.noreply.github.com>
2023-03-20 05:37:36 +0000

    added comments to postcreate script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,3 +1,8 @@
+# Install the package including all development dependencies
 pip install -e .[dev] --user
+
+# Copy the config sample file
 cp ./scripts/config.sample.py ./scripts/config.py
+
+# Copy the credentials sample file
 cp ./scripts/credentials
